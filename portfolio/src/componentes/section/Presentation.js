3b87cb5a94fac76cc2db6f9aa90ef6b937304e87
wagner1067 <barra-wag@hotmail.com>
2024-01-31 22:34:47 -0300

    versão 2

--- portfolio/src/componentes/section/Presentation.js
+++ portfolio/src/componentes/section/Presentation.js
@@ -1,11 +1,46 @@
 import styles from './Presentation.module.css';
 import ButtonA from '../elements/ButtonA';
+import { useEffect, useState } from 'react';
 
 function Presentation() {
+
+    const [text, setText] = useState('');
+    const toRotate = ['Wagner Oliveira', 'Desenvolvedor Front End', 'Desenvolvedor Back End', 'Full Stack'];
+    const [loop, setLoop] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const period = 100;
+    const [delta, setDelta] = useState(100)
+
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            toType()
+        }, delta)
+        return () => { clearInterval(ticker) }
+
+    }, [text])
+
+    const toType = () => {
+        let i = loop % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setDelta(period);
+            setLoop(loop + 1);
+        }
+    }
+
     return (
         <div className={styles.presentation} id="Presentation">
             <h4><strong> Bem-vindo ao meu Portfólio</strong></h4>
-            <h1>Olá, eu sou Wagner Oliveira</h1>
+            <h1>Olá, eu sou {text}</h1>
             <p>
                 Sou apaixonado por tecnologia e soluções inovadoras.<br />
                 Estou cursando tecnologia na Escola DNC, esse portfólio <br />
@@ -13,8 +48,8 @@ function Presentation() {
                 particulares e do curso. Atualmente trabalho num provedor <br />
                 de internet.
             </p>
-            <ButtonA link= 'https://github.com/wagner1067'  text='Conecte-se comigo!'/>
-            
+            <ButtonA link='https://github.com/wagner1067' text='Conecte-se comigo!' />
+
         </div>
     )
 }
