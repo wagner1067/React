f9862115753be06a44c7b34e3fca1fb9ba25819d
wagner1067 <barra-wag@hotmail.com>
2024-01-31 22:38:11 -0300

    Terminado versão 2

--- portfolio/src/componentes/section/Presentation.js
+++ portfolio/src/componentes/section/Presentation.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 function Presentation() {
 
     const [text, setText] = useState('');
-    const toRotate = ['Wagner Oliveira', 'Desenvolvedor Front End', 'Desenvolvedor Back End', 'Full Stack'];
+    const toRotate = ['Wagner Oliveira!', 'Desenvolvedor Front End!', 'Desenvolvedor Back End!', 'Full Stack!'];
     const [loop, setLoop] = useState(0);
     const [isDeleting, setIsDeleting] = useState(false);
     const period = 100;
